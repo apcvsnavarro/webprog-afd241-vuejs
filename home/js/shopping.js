4add4cf477ade472df8a26025c9c34d2061f7dff
Angelo <97135832+apcvsnavarro@users.noreply.github.com>
2025-04-29 14:44:51 +0800

    ap p3

--- home/js/shopping.js
+++ home/js/shopping.js
@@ -1,8 +1,7 @@
-// shopping.js
 const app3 = Vue.createApp({
   data() {
     return {
-      itemName: '',
+      itemName: null,
       itemNumber: null,
       itemImportant: false,
       shoppingList: [
@@ -10,21 +9,21 @@ const app3 = Vue.createApp({
         { name: 'Bread', number: 1, important: false, found: false },
         { name: 'Soap', number: 1, important: true, found: true }
       ]
-    };
+    }
   },
   methods: {
-    addItem() {
-      if (!this.itemName || !this.itemNumber) return;
-      this.shoppingList.push({
+    addItem(){
+      let item = {
         name: this.itemName,
         number: this.itemNumber,
         important: this.itemImportant,
         found: false
-      });
-      this.itemName = '';
-      this.itemNumber = null;
-      this.itemImportant = false;
+      }
+      this.shoppingList.push(item)
+      this.itemName = null
+      this.itemNumber = null  
+      this.itemImportant = false
     }
   }
-});
-app3.mount('#app3');
+})
+app3.mount('#app3')
