300f2362efd6cda20cee15aae889294362d8504e
Angelo <97135832+apcvsnavarro@users.noreply.github.com>
2025-04-29 15:20:41 +0800

    again farther revert

--- home/js/vue.js
+++ home/js/vue.js
@@ -1,29 +1,10 @@
-const app3 = Vue.createApp({
+const app = Vue.createApp({
   data() {
-    return {
-      itemName: null,
-      itemNumber: null,
-      itemImportant: false,
-      shoppingList: [
-        { name: 'Tomatoes', number: 5, important: false, found: false },
-        { name: 'Bread', number: 1, important: false, found: false },
-        { name: 'Soap', number: 1, important: true, found: true }
-      ]
-    };
-  },
-  methods: {
-    addItem() {
-      if (!this.itemName || !this.itemNumber) return;
-      this.shoppingList.push({
-        name: this.itemName,
-        number: this.itemNumber,
-        important: this.itemImportant,
-        found: false
-      });
-      this.itemName = null;
-      this.itemNumber = null;
-      this.itemImportant = false;
-    }
+   return {
+    message: "Hello to my World~",
+    vueClass: "pinkBG"
+   }
   }
-});
-app3.mount('#app3');
+ })
+ 
+ app.mount('#app')
