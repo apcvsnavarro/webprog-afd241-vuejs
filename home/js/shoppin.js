d989b5dd1b4e83c7df1f3c1da56d7af01a08bdca
Angelo <97135832+apcvsnavarro@users.noreply.github.com>
2025-04-29 19:52:33 +0800

    perpexity codes

---
+++ home/js/shoppin.js
@@ -0,0 +1,30 @@
+// shopping.js
+const app3 = Vue.createApp({
+  data() {
+    return {
+      itemName: '',
+      itemNumber: null,
+      itemImportant: false,
+      shoppingList: [
+        { name: 'Tomatoes', number: 5, important: false, found: false },
+        { name: 'Bread', number: 1, important: false, found: false },
+        { name: 'Soap', number: 1, important: true, found: true }
+      ]
+    };
+  },
+  methods: {
+    addItem() {
+      if (!this.itemName || !this.itemNumber) return;
+      this.shoppingList.push({
+        name: this.itemName,
+        number: this.itemNumber,
+        important: this.itemImportant,
+        found: false
+      });
+      this.itemName = '';
+      this.itemNumber = null;
+      this.itemImportant = false;
+    }
+  }
+});
+app3.mount('#app3');
--- home/js/vue.js
+++ home/js/vue.js
@@ -1,9 +1,10 @@
+// vue.js
 const app = Vue.createApp({
   data() {
-   return {
-    message: "Welcome to my World!",
-    vueClass: "pinkBG"
-   }
+    return {
+      message: "Welcome to my World!",
+      vueClass: "pinkBG"
+    };
   }
- })
- app.mount('#app')
+});
+app.mount('#app');
